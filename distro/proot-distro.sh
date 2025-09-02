7a12732eb4c48f71ceb8c4d4728d01337c2df4f5
Prabesh <malladipa142@gmail.com>
2025-09-02 22:01:36 +0545

    Update proot-distro.sh

--- distro/proot-distro.sh
+++ distro/proot-distro.sh
@@ -1,18 +1,25 @@
 #!/data/data/com.termux/files/usr/bin/bash
-DISTRO_NAME="ubuntu-modded"
+: <<'BANNER'
+  _________________  .____    ________     ____ _____________ ____ _________________________ ___ 
+ /   _____/\_____  \ |    |   \_____  \   |    |   \______   \    |   \      \__    ___/    |   \
+ \_____  \  /   |   \|    |    /   |   \  |    |   /|    |  _/    |   /   |   \|    |  |    |   /
+ /        \/    |    \    |___/    |    \ |    |  / |    |   \    |  /    |    \    |  |    |  / 
+/_______  /\_______  /_______ \_______  / |______/  |______  /______/\____|__  /____|  |______/  
+        \/         \/        \/       \/                   \/                \/                  
+BANNER
 
-echo "[+] Installing Ubuntu if not already installed..."
-if ! proot-distro list | grep -q "$DISTRO_NAME"; then
-    proot-distro install ubuntu
-    echo "[+] Ubuntu installed!"
+# Install Ubuntu in Termux for solo-modded-ubuntu
+
+DISTRO_NAME="ubuntu-solo"
+
+if proot-distro list | grep -q "$DISTRO_NAME"; then
+    echo "[!] $DISTRO_NAME already installed."
 else
-    echo "[!] Ubuntu already installed."
+    echo "[+] Installing $DISTRO_NAME..."
+    proot-distro install ubuntu
+    mv ~/../usr/var/lib/proot-distro/installed-rootfs/ubuntu ~/../usr/var/lib/proot-distro/installed-rootfs/$DISTRO_NAME
+    echo "[+] $DISTRO_NAME installed."
 fi
 
-echo "[+] Updating Ubuntu packages..."
-proot-distro login ubuntu -- bash -c "apt update && apt upgrade -y"
-
-echo "[+] Installing GUI packages..."
-proot-distro login ubuntu -- bash -c "apt install -y xfce4 xfce4-goodies x11vnc firefox dbus-x11"
-
-echo "[+] GUI environment setup complete!"
+echo "[+] Updating and installing GUI packages..."
+proot-distro login $DISTRO_NAME -- bash -c "apt update && apt upgrade -y && apt install -y xfce4 xfce4-goodies x11vnc firefox dbus-x11"
