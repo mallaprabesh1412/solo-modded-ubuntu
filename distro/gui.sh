42158ab02f7708d60fc2445e6fb580ef87fda3c1
Prabesh <malladipa142@gmail.com>
2025-09-02 22:02:17 +0545

    Update gui.sh

--- distro/gui.sh
+++ distro/gui.sh
@@ -1,5 +1,18 @@
 #!/data/data/com.termux/files/usr/bin/bash
+: <<'BANNER'
+  _________________  .____    ________     ____ _____________ ____ _________________________ ___ 
+ /   _____/\_____  \ |    |   \_____  \   |    |   \______   \    |   \      \__    ___/    |   \
+ \_____  \  /   |   \|    |    /   |   \  |    |   /|    |  _/    |   /   |   \|    |  |    |   /
+ /        \/    |    \    |___/    |    \ |    |  / |    |   \    |  /    |    \    |  |    |  / 
+/_______  /\_______  /_______ \_______  / |______/  |______  /______/\____|__  /____|  |______/  
+        \/         \/        \/       \/                   \/                \/                  
+BANNER
+
+# Start Ubuntu GUI using VNC
+
+DISTRO_NAME="ubuntu-solo"
+
 echo "[+] Starting VNC server..."
-proot-distro login ubuntu -- bash -c "mkdir -p ~/.vnc && x11vnc -storepasswd termux ~/.vnc/passwd"
-proot-distro login ubuntu -- bash -c "vncserver :1 -geometry 1280x720 -depth 24"
-echo "[+] GUI running! Connect using VNC Viewer at localhost:5901"
+proot-distro login $DISTRO_NAME -- bash -c "mkdir -p ~/.vnc && x11vnc -storepasswd termux ~/.vnc/passwd"
+proot-distro login $DISTRO_NAME -- bash -c "vncserver :1 -geometry 1280x720 -depth 24"
+echo "[+] GUI running! Connect via VNC Viewer at localhost:5901"
