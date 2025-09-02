62bdc890dc5ed79b8b49add19a5fbd41fe6fbb6a
Prabesh <malladipa142@gmail.com>
2025-09-02 22:03:27 +0545

    Update user.sh

--- distro/user.sh
+++ distro/user.sh
@@ -1,4 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo "[+] Creating default user 'termuxuser'..."
-proot-distro login ubuntu -- bash -c "useradd -m termuxuser || true; echo 'termuxuser:termux' | chpasswd"
+: <<'BANNER'
+  _________________  .____    ________     ____ _____________ ____ _________________________ ___ 
+ /   _____/\_____  \ |    |   \_____  \   |    |   \______   \    |   \      \__    ___/    |   \
+ \_____  \  /   |   \|    |    /   |   \  |    |   /|    |  _/    |   /   |   \|    |  |    |   /
+ /        \/    |    \    |___/    |    \ |    |  / |    |   \    |  /    |    \    |  |    |  / 
+/_______  /\_______  /_______ \_______  / |______/  |______  /______/\____|__  /____|  |______/  
+        \/         \/        \/       \/                   \/                \/                  
+BANNER
+
+# Create default user inside Ubuntu
+
+DISTRO_NAME="ubuntu-solo"
+
+echo "[+] Setting up default user..."
+proot-distro login $DISTRO_NAME -- bash -c "useradd -m termuxuser || true; echo 'termuxuser:termux' | chpasswd"
 echo "[+] User setup complete!"
