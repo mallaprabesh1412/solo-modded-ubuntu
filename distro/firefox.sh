bb3322ef4e8c4590194d8683bf7b7b5c52c52f77
Prabesh <malladipa142@gmail.com>
2025-09-02 22:51:36 +0545

    Update firefox.sh

--- distro/firefox.sh
+++ distro/firefox.sh
@@ -1,16 +1,2 @@
-#!/data/data/com.termux/files/usr/bin/bash
-: <<'BANNER'
-  _________________  .____    ________     ____ _____________ ____ _________________________ ___ 
- /   _____/\_____  \ |    |   \_____  \   |    |   \______   \    |   \      \__    ___/    |   \
- \_____  \  /   |   \|    |    /   |   \  |    |   /|    |  _/    |   /   |   \|    |  |    |   /
- /        \/    |    \    |___/    |    \ |    |  / |    |   \    |  /    |    \    |  |    |  / 
-/_______  /\_______  /_______ \_______  / |______/  |______  /______/\____|__  /____|  |______/  
-        \/         \/        \/       \/                   \/                \/                  
-BANNER
-
-# Launch Firefox in Ubuntu GUI
-
-DISTRO_NAME="ubuntu-solo"
-
-echo "[+] Launching Firefox..."
-proot-distro login $DISTRO_NAME -- bash -c "DISPLAY=:1 firefox &"
+#!/bin/bash
+firefox &
